38b117ffb7a8f64b5c28dd35bacb36acac8d5e09
Alice Bedow <alice.bedow@adesso.de>
2024-06-17 16:15:15 +0200

    Repo aufgeraeumt

--- spring-batch/src/main/java/de/alice/springbatch/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package de.alice.springbatch.repository;
-
-import de.alice.springbatch.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
